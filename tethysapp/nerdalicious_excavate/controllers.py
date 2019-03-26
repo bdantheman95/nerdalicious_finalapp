554220ec462acdf57f27ca4dca9c8fda5cec9839
Brandon <46941942+bdantheman95@users.noreply.github.com>
2019-03-25 23:55:23 -0600

    Add files via upload

--- tethysapp/nerdalicious_excavate/controllers.py
+++ tethysapp/nerdalicious_excavate/controllers.py
@@ -7,68 +7,33 @@ def home(request):
     """
     Controller for the app home page.
     """
-    save_button = Button(
-        display_text='',
-        name='save-button',
-        icon='glyphicon glyphicon-floppy-disk',
-        style='success',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Save'
-        }
-    )
-
-    edit_button = Button(
-        display_text='',
-        name='edit-button',
-        icon='glyphicon glyphicon-edit',
-        style='warning',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Edit'
-        }
-    )
-
-    remove_button = Button(
-        display_text='',
-        name='remove-button',
-        icon='glyphicon glyphicon-remove',
-        style='danger',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Remove'
-        }
-    )
-
-    previous_button = Button(
-        display_text='Previous',
-        name='previous-button',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Previous'
-        }
-    )
-
-    next_button = Button(
-        display_text='Next',
-        name='next-button',
-        attributes={
-            'data-toggle':'tooltip',
-            'data-placement':'top',
-            'title':'Next'
-        }
-    )
-
-    context = {
-        'save_button': save_button,
-        'edit_button': edit_button,
-        'remove_button': remove_button,
-        'previous_button': previous_button,
-        'next_button': next_button
-    }
+    context = {}
 
     return render(request, 'nerdalicious_excavate/home.html', context)
+
+
+def mockup(request):
+    """
+    Controller for the app home page.
+    """
+    context = {}
+
+    return render(request, 'nerdalicious_excavate/mockup.html', context)
+
+
+def proposal(request):
+    """
+    Controller for the app home page.
+    """
+    context = {}
+
+    return render(request, 'nerdalicious_excavate/proposal.html', context)
+
+
+def geoproc(request):
+    """
+    Controller for the app home page.
+    """
+    context = {}
+
+    return render(request, 'nerdalicious_excavate/geoproc.html', context)
--- tethysapp/tethysapp-nerdalicious_excavate/tethysapp/nerdalicious_excavate/app.py
+++ tethysapp/tethysapp-nerdalicious_excavate/tethysapp/nerdalicious_excavate/app.py
@@ -8,12 +8,12 @@ class NerdaliciousExcavate(TethysAppBase):
 
     name = 'Nerdalicious Excavate'
     index = 'nerdalicious_excavate:home'
-    icon = 'nerdalicious_excavate/images/diagram.png'
+    icon = 'nerdalicious_excavate/images/icon.gif'
     package = 'nerdalicious_excavate'
-    root_url = 'nerdalicious_excavate'
+    root_url = 'nerdalicious-excavate'
     color = '#5b3b26'
     description = 'Used to find seepage and slope stability'
-    tags = 'excavate,seep,stability'
+    tags = 'excavate, seepage, slopestability'
     enable_feedback = False
     feedback_emails = []
 
@@ -28,21 +28,6 @@ class NerdaliciousExcavate(TethysAppBase):
                 name='home',
                 url='nerdalicious-excavate',
                 controller='nerdalicious_excavate.controllers.home'
-            ),
-            UrlMap(
-                name='mockup',
-                url='nerdalicious-excavate/mockup',
-                controller='nerdalicious_excavate.controllers.mockup'
-            ),
-            UrlMap(
-                name='proposal',
-                url='nerdalicious-excavate/proposal',
-                controller='nerdalicious_excavate.controllers.proposal'
-            ),
-            UrlMap(
-                name='geoproc',
-                url='nerdalicious-excavate/geoproc',
-                controller='nerdalicious_excavate.controllers.geoproc'
             ),
         )
 
